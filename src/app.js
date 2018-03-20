89fef624cdb1718c468b2616d1c15acdc177d3d7
unknown <fncoder@gmail.com>
2018-03-20 21:45:08 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyle from './scss/global.scss';
+import renderHTML from './index.html';
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractCSS = new ExtractTextPlugin('./css/global.min.css')
+const WriteFilePlugin = require('write-file-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: ['./src/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js'
+  },
+  devServer: {
+    contentBase: 'dist',
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractCSS.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    ExtractCSS,
+    new UglifyJsPlugin(),
+    new WriteFilePlugin,
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html'
+    })
+  ]
+};
